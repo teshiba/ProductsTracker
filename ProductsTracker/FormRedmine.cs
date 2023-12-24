fa58a04005514c4c10f3df2851c11ccabec5e29e
Satoshi TESHIBA <teshiba.satoshi@gmail.com>
2023-12-24 16:01:10 +0900

    add project files

---
+++ ProductsTracker/FormRedmine.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductsTracker;
+
+using System.Windows.Forms;
+
+/// <summary>
+/// Show Redmine properties form class.
+/// </summary>
+public partial class FormRedmine : Form
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FormRedmine"/> class.
+    /// </summary>
+    public FormRedmine() => InitializeComponent();
+}
---
+++ ProductsTracker/Program.cs
@@ -0,0 +1,19 @@
+namespace ProductsTracker;
+
+/// <summary>
+/// Entry point of the program.
+/// </summary>
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    private static void Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new FormRedmine());
+    }
+}
